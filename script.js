2c542819aa667f31a4c833f8ccd5b8617dc18e22
Prachi Bangar <prachibangar04@gmail.com>
2026-09-09 22:47:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMessage() {
+
+    alert("Thank you for visiting my portfolio! Have a great day.");
+
+}
+
+
+window.addEventListener("load", function() {
+
+    console.log("Prachi's Portfolio Loaded Successfully!");
+
+});
